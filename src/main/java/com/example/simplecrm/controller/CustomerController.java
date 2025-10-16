03d1cf089f4a9f911856280c7cbe5b3743b7e0c6
kxmikot <Daniils.mironovs@edu.rtu.lv>
2025-10-16 08:39:15 +0300

    ADD: "ApiResponse" for one response style(json)

--- src/main/java/com/example/simplecrm/controller/CustomerController.java
+++ src/main/java/com/example/simplecrm/controller/CustomerController.java
@@ -4,12 +4,13 @@ import com.example.simplecrm.dto.CustomerRequestDTO;
 import com.example.simplecrm.dto.CustomerResponseDTO;
 import com.example.simplecrm.service.CustomerService;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import com.example.simplecrm.api.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 
 @RestController
@@ -26,36 +27,66 @@ public class CustomerController {
     @Operation(summary = "Add new customer")
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public CustomerResponseDTO createCustomer(@Valid @RequestBody CustomerRequestDTO dto) {
-        return customerService.createCustomer(dto);
+    public ResponseEntity<ApiResponse<CustomerResponseDTO>> createCustomer(@Valid @RequestBody CustomerRequestDTO dto) {
+        CustomerResponseDTO response = customerService.createCustomer(dto);
+        return ResponseEntity.ok(
+                ApiResponse.<CustomerResponseDTO>builder()
+                        .status(HttpStatus.CREATED.value())
+                        .message("Customer created successfully")
+                        .data(response)
+                        .build()
+        );
     }
 
     @Operation(summary = "Get customer by ID")
     @GetMapping("/{id}")
-    public CustomerResponseDTO getCustomerById(@PathVariable Long id) {
-        return customerService.getCustomerById(id);
+    public ResponseEntity<ApiResponse<CustomerResponseDTO>> getCustomerById(@PathVariable Long id) {
+        CustomerResponseDTO dto = customerService.getCustomerById(id);
+        return ResponseEntity.ok(
+            ApiResponse.<CustomerResponseDTO>builder()
+                    .status(HttpStatus.OK.value())
+                    .message("Customer fetched successfully")
+                    .data(dto)
+                    .build()
+        );
     }
 
     @Operation(summary = "Delete customer by ID")
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void deleteCustomer(@PathVariable Long id) {
+    public ResponseEntity<ApiResponse<Void>> deleteCustomer(@PathVariable Long id) {
         customerService.deleteCustomer(id);
+        ApiResponse<Void> response = ApiResponse.<Void>builder()
+                .status(HttpStatus.OK.value())
+                .message("Customer deleted successfully")
+                .data(null)
+                .build();
+        return ResponseEntity.ok(response);
     }
 
     @Operation(summary = "Update existing customer data")
     @PutMapping("/{id}")
-    public CustomerResponseDTO updateCustomer(@Valid @RequestBody CustomerRequestDTO dto, @PathVariable Long id) {
-        return customerService.updateCustomer(dto, id);
+    public ResponseEntity<ApiResponse<CustomerResponseDTO>> updateCustomer(@Valid @RequestBody CustomerRequestDTO dto, @PathVariable Long id) {
+        CustomerResponseDTO response = customerService.updateCustomer(dto, id);
+        return ResponseEntity.ok(
+                ApiResponse.<CustomerResponseDTO>builder()
+                        .status(HttpStatus.OK.value())
+                        .message("Customer updated successfully")
+                        .data(response)
+                        .build()
+        );
     }
 
     @Operation(summary = "Get all customers")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Success"),
-            @ApiResponse(responseCode = "403", description = "You don't have permission")
-    })
     @GetMapping
-    public List<CustomerResponseDTO> getAllCustomers() {
-        return customerService.getAllCustomers();
+    public ResponseEntity<ApiResponse<List<CustomerResponseDTO>>> getAllCustomers() {
+        List<CustomerResponseDTO> customers = customerService.getAllCustomers();
+
+        ApiResponse<List<CustomerResponseDTO>> response = ApiResponse.<List<CustomerResponseDTO>>builder()
+                .status(HttpStatus.OK.value())
+                .message("Customer list")
+                .data(customers)
+                .build();
+        return ResponseEntity.ok(response);
     }
 }
--- src/main/java/com/example/simplecrm/mapper/CustomerMapper.java
+++ src/main/java/com/example/simplecrm/mapper/CustomerMapper.java
@@ -3,17 +3,15 @@ package com.example.simplecrm.mapper;
 import com.example.simplecrm.dto.CustomerRequestDTO;
 import com.example.simplecrm.dto.CustomerResponseDTO;
 import com.example.simplecrm.model.Customer;
+import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class CustomerMapper {
 
     private final ModelMapper mapper;
-
-    public CustomerMapper() {
-        this.mapper = new ModelMapper();
-    }
 
     public Customer toEntity(CustomerRequestDTO customerRequestDTO) {
         return mapper.map(customerRequestDTO, Customer.class);
